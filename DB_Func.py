036b30afc0b3765e7e7520e44b845ae3b1af54d3
Henry Baldacci <henry.louis.980@gmail.com>
2021-07-07 18:56:14 +0000

    Basic DB functions

--- DB_Func.py
+++ DB_Func.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from mysql.connector import connect, Error
 
 class DB_Func:
@@ -7,45 +8,47 @@ class DB_Func:
         self.connection = connect(
             host=self.host,
             user=self.user,
-            #password=getpass("Enter password: "),
+            password = 'Password',
         )
+        self.db = None
+        self.df = pd.DataFrame()
 
 
     def createDB(self, db_name):
         try:
+            self.db = db_name
             create_db_query = 'CREATE DATABASE IF NOT EXISTS ' + db_name + '; '
-            with self.con.cursor() as cursor:
+            use_database_entry = "USE " + self.db
+            
+            with self.connection.cursor() as cursor:
                 cursor.execute(create_db_query)
-
-                self.connection = connect(
-                    host = self.host,
-                    user = self.user,
-                    #password=getpass("Enter password: "),
-                    database = db_name,
-                )
-                connection.commit()
-
+                cursor.execute(use_database_entry)
+                self.connection.commit()
+                
         except Error as e:
             print(e)
 
     def addEntry(self, table, number):
         create_table_query = """
-        CREATE TABLE [IF NOT EXISTS] {} (
-            number INT PRIMARY KEY,
-            appearances INT UNSIGNED DEFAULT 1
+        CREATE TABLE IF NOT EXISTS {} (
+            number VARCHAR(255) NOT NULL,
+            appearances INT UNSIGNED DEFAULT 1,
+            UNIQUE KEY (number)
         )
         """.format(table)
         insert_entry = """
         INSERT INTO {} (number) 
-            VALUES number
+            VALUES ({})
             ON DUPLICATE KEY UPDATE appearances = appearances + 1 
-        """.format(table)
+        """.format(table, number)
 
-        try:
-            with self.connection.cursor() as cursor:
-                cursor.execute(create_table_query)
-                cursor.execute(insert_entry)
-                connection.commit()
-        
-        except Error as e:
-            print(e)
+        with self.connection.cursor() as cursor:
+            cursor.execute(create_table_query)
+            cursor.execute(insert_entry)
+            self.connection.commit()
+
+
+    def showTable(self, table):
+        show_table_query = 'SELECT * FROM ' + table
+        df = pd.read_sql(show_table_query, con=self.connection)
+        print(df)
--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 # Import necessary modules
 import requests
-
+from DB_Func import DB_Func
 
 class MathBites:
     def __init__(self):
         self.KEY = "8ea42e22ddmsh636295d97820b16p13f8fajsn0f1762f562d0"
-
+        self.dbinter = DB_Func("localhost", "root")
+        self.dbinter.createDB('math_bites')
+    
+    def main_menu(self):
+        choice = input("""
+            What would you like to do?
+            (1) -- Look up a fact 
+            (2) -- View previous searches
+            (0) -- Quit
+            Enter choice: """)
+        
+        if choice == '1':
+            self.fact_menu()
+        
+        elif choice == '2':
+            self.history_menu()
+        
+        elif choice == '0':
+            print("Bye!")
+            
+        else:
+            self.main_menu()
 
     def fact_menu(self):
         choice = input("""
@@ -52,6 +73,8 @@ class MathBites:
 
         else:
             self.fact_menu()
+        
+        self.main_menu()
 
 
     def math_lookup(self, number):
@@ -60,6 +83,7 @@ class MathBites:
 
         print("{} is {}.".format(response_data['number'],
                                  response_data['text']))
+        self.dbinter.addEntry('math', number)
 
 
     def trivia_lookup(self, number):
@@ -68,6 +92,7 @@ class MathBites:
 
         print("{} is {}.".format(response_data['number'],
                                  response_data['text']))
+        self.dbinter.addEntry('trivia', number)
 
 
     def year_lookup(self, number):
@@ -76,16 +101,18 @@ class MathBites:
 
         print("{} is the year {}.".format(response_data['number'],
                                           response_data['text']))
-
+        self.dbinter.addEntry('year', number)
+        
 
     def date_lookup(self, day, month):
         url = "https://numbersapi.p.rapidapi.com/" + month + "/" + day + "/date"
         response_data = self.get_data(url).json()
-
+        
         print("On {}/{}/{}, {}.".format(month,
                                         day,
                                         response_data['year'],
                                         response_data['text']))
+        self.dbinter.addEntry('date', '{}/{}'.format(month, day))
 
 
     def get_data(self, url):
@@ -98,6 +125,33 @@ class MathBites:
         return requests.request("GET", url, headers=HEADERS, params=QUERYSTRING)
 
 
+    def history_menu(self):        
+        choice = input("""
+            What search history would you like to see?
+            (1) -- Math history
+            (2) -- Trivia history
+            (3) -- Year history
+            (4) -- Date history
+            Enter choice: """)
+
+        if choice == '1':
+            self.dbinter.showTable('math')
+
+        elif choice == '2':
+            self.dbinter.showTable('trivia')
+
+        elif choice == '3':
+            self.dbinter.showTable('year')
+
+        elif choice == '4':
+            self.dbinter.showTable('date')
+        
+        else:
+            self.history_menu()
+        
+        self.main_menu()
+
+
 if __name__ == "__main__":
     main = MathBites()
-    main.fact_menu()
+    main.main_menu()
