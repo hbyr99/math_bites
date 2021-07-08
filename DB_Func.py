617ded35bda7dc033bcd838d08de14367af219e6
Henry Baldacci <henry.louis.980@gmail.com>
2021-07-08 19:18:27 +0000

    Analytical data representation

--- DB_Func.py
+++ DB_Func.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import plotly.graph_objects as go
 import warnings
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+from sqlalchemy.exc import ProgrammingError
 
 
 class DB_Func:
@@ -26,14 +28,14 @@ class DB_Func:
     def addEntry(self, table, number):
         create_table_query = text("""
         CREATE TABLE IF NOT EXISTS {} (
-            number VARCHAR(255) NOT NULL,
+            entry INT NOT NULL,
             appearances INT UNSIGNED DEFAULT 1,
-            UNIQUE KEY (number)
+            UNIQUE KEY (entry)
         )
         """.format(table))
         insert_entry = text("""
-        INSERT INTO {} (number) 
-            VALUES ({})
+        INSERT INTO {} (entry) 
+            VALUES ('{}')
             ON DUPLICATE KEY UPDATE appearances = appearances + 1 
         """.format(table, number))
         
@@ -45,5 +47,34 @@ class DB_Func:
 
     def showTable(self, table):
         show_table_query = 'SELECT * FROM ' + table
-        df = pd.read_sql(show_table_query, con=self.connection)
-        print(df)
+        try:
+            df = pd.read_sql(show_table_query, con=self.connection)
+            print(df)
+        except ProgrammingError as e:
+            print("No search history found! ")
+
+
+    def showScatter(self, table):
+        fetch_table_query = 'SELECT * FROM ' + table
+        try:
+            df = pd.read_sql(fetch_table_query, con=self.connection)
+        except ProgrammingError as e:
+            print("No search history found! ")
+            return
+
+        fig = go.Figure(data = go.Scatter(x = df['entry'],
+                                          y = df['appearances'],
+                                          mode = 'markers',
+                                          marker_color = df['appearances'],
+                                          text = df['entry']))
+        fig.update_layout(title = table.capitalize())
+        fig.write_html(table + '.html')
+        print('Check folder for {}.html'.format(table))
+
+
+    def deleteDB(self, db_name):
+        delete_db_query = text('DROP DATABASE ' + db_name)
+        
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", module = "sqlalchemy")
+            self.connection.execute(delete_db_query)
--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class MathBites:
                 What would you like to do?
                 (1) -- Look up a fact 
                 (2) -- View previous searches
+                (3) -- View data analytics
+                (4) -- Clear history
                 (0) -- Quit
                 Enter choice: """)
 
@@ -22,6 +24,13 @@ class MathBites:
 
             elif choice == '2':
                 self.history_menu()
+            
+            elif choice == '3':
+                self.data_menu()
+
+            elif choice == '4':
+                self.dbinter.deleteDB('math_bites')
+                self.dbinter.createDB('math_bites')
 
             elif choice == '0':
                 print("Bye!")
@@ -36,11 +45,10 @@ class MathBites:
             choice = input("""
                 What kind of fact would you like to look up?
                 (1) -- Math fact
-                (2) -- Trivia fact
-                (3) -- Random fact
-                (4) -- Year fact
-                (5) -- Date fact
-                (6) -- Text fact (EXPERIMENTAL!)
+                (2) -- Trivia fact ('random' for a random fact!)
+                (3) -- Year fact
+                (4) -- Date fact
+                (5) -- Text fact (EXPERIMENTAL!)
                 (0) -- Return to main menu
                 Enter choice: """)
 
@@ -53,24 +61,20 @@ class MathBites:
                 self.trivia_lookup(entry)
 
             elif choice == '3':
-                self.trivia_lookup('random')
-
-            elif choice == '4':
                 entry = input("What year would you like to look up? ")
                 self.year_lookup(entry)
 
-            elif choice == '5':
+            elif choice == '4':
                 entry_month = input(
                     "What month would you like to look up? (Please enter number) ")
                 entry_day = input(
                     "What day would you like to look up? (Please enter number) ")
+
                 self.date_lookup(entry_day, entry_month)
 
-            elif choice == '6':
+            elif choice == '5':
                 entry = input("What text would you like to look up? ")
-
                 text_num = 0
-
                 for letter in entry:
                     text_num += ord(letter)
 
@@ -90,6 +94,7 @@ class MathBites:
         print("{} is {}.".format(response_data['number'],
                                  response_data['text']))
         self.dbinter.addEntry('math', number)
+        self.dbinter.addEntry('main', number)
 
 
     def trivia_lookup(self, number):
@@ -108,17 +113,20 @@ class MathBites:
         print("{} is the year {}.".format(response_data['number'],
                                           response_data['text']))
         self.dbinter.addEntry('year', number)
-        
+        self.dbinter.addEntry('main', number)
+
 
     def date_lookup(self, day, month):
         url = "https://numbersapi.p.rapidapi.com/" + month + "/" + day + "/date"
         response_data = self.get_data(url).json()
-        
-        print("On {}/{}/{}, {}.".format(month,
+
+        print("On {}/{}/{}, the {} day of the year, {}.".format(month,
                                         day,
                                         response_data['year'],
+                                        response_data['number'],
                                         response_data['text']))
-        self.dbinter.addEntry('date', '{}/{}'.format(month, day))
+        self.dbinter.addEntry('date', '{}'.format(response_data['number']))
+        self.dbinter.addEntry('main', '{}'.format(response_data['number']))
 
 
     def get_data(self, url):
@@ -135,24 +143,58 @@ class MathBites:
         while True:
             choice = input("""
                 What search history would you like to see?
-                (1) -- Math history
-                (2) -- Trivia history
-                (3) -- Year history
-                (4) -- Date history
+                (1) -- Main history
+                (2) -- Math history
+                (3) -- Trivia history
+                (4) -- Year history
+                (5) -- Date history
                 (0) -- Return to main menu
                 Enter choice: """)
 
             if choice == '1':
-                self.dbinter.showTable('math')
+                self.dbinter.showTable('main')
 
             elif choice == '2':
-                self.dbinter.showTable('trivia')
+                self.dbinter.showTable('math')
 
             elif choice == '3':
-                self.dbinter.showTable('year')
+                self.dbinter.showTable('trivia')
 
             elif choice == '4':
+                self.dbinter.showTable('year')
+
+            elif choice == '5':
                 self.dbinter.showTable('date')
+
+            elif choice == '0':
+                break
+
+            else:
+                print("Invalid input! ")
+
+
+    def data_menu(self):
+        while True:
+            choice = input("""
+                What information would you like displayed?
+                (1) -- Mathematics
+                (2) -- Trivia
+                (3) -- Year
+                (4) -- Date
+                (0) -- Return to previous menu
+                Enter choice: """)
+
+            if choice == '1':
+                self.dbinter.showScatter('math')
+
+            elif choice == '2':
+                self.dbinter.showScatter('trivia')
+
+            elif choice == '3':
+                self.dbinter.showScatter('year')
+
+            elif choice == '4':
+                self.dbinter.showScatter('date')
 
             elif choice == '0':
                 break
